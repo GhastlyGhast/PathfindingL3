f3d39346f765432e8e32085dbed6ae152a1129af
Virgil Marionneau <marionneau.virgil@gmail.com>
2023-02-27 15:19:34 +0100

    Added real window display

--- src/main.jl
+++ src/main.jl
@@ -1,3 +1,6 @@
+using Colors
+using ImageView
+using Gtk.ShortNames 
 
 module Tiles
     export TileType, Unpassable, Terrain, Swamp, Water
@@ -63,6 +66,19 @@ txt_colors =
         'T' => :yellow
         )
 
+img_colors = 
+    Dict(
+        'X' => RGB(0.0,1.0,1.0),
+        '+' => RGB(0.0,1.0,0.0),
+        '-' => RGB(1.0,0.0,0.0),
+        '@' => RGB(0.0,0.0,0.0),
+        'O' => RGB(0.0,0.0,0.0),
+        '.' => RGB(.5,.5,.5),
+        'G' => RGB(.5,.5,.5),
+        'S' => RGB(1.0,.5,0.0),
+        'W' => RGB(0.0,0.0,1.0),
+        'T' => RGB(1.0,1.0,0.0)
+        )
 
 if haskey(algo_dict, algorithm)
     println("Launching search using " * algorithm)
@@ -80,6 +96,17 @@ if haskey(algo_dict, algorithm)
         end
         print('\n')
     end
+    img = (c -> img_colors[c]).(text_map)
+    cond = Condition()
+    shw = imshow(img)
+    win = shw["gui"]["window"]
+    signal_connect(win, :destroy) do widget
+        notify(cond)
+    end
+
+    wait(cond)
+
 else
     error("Not a valid algorithm : " * algorithm)
 end
+
