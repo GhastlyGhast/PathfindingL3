b540b36dfa14e8863b595c7bac482d80d6365bb2
Virgil Marionneau <marionneau.virgil@gmail.com>
2023-02-27 14:15:17 +0100

    Added some color

--- src/main.jl
+++ src/main.jl
@@ -48,6 +48,22 @@ println("Map Loaded.")
 
 ymax,xmax = size(map)
 
+
+txt_colors = 
+    Dict(
+        'X' => :cyan,
+        '+' => :green,
+        '-' => :red,
+        '@' => :white,
+        'O' => :white,
+        '.' => :black,
+        'G' => :black,
+        'S' => :orange,
+        'W' => :blue,
+        'T' => :yellow
+        )
+
+
 if haskey(algo_dict, algorithm)
     println("Launching search using " * algorithm)
     path = algo_dict[algorithm](map, start, target)
@@ -56,9 +72,13 @@ if haskey(algo_dict, algorithm)
         text_map[y,x] = 'X'
     end
     text_map[starty,startx] = '+'
-    text_map[targety,targetx] = '!'
+    text_map[targety,targetx] = '-'
     for y in 1:ymax
-        println(String(text_map[y,:]))
+        for x in 1:xmax
+            c = text_map[y,x]
+            printstyled(stdout, c, color = txt_colors[c])
+        end
+        print('\n')
     end
 else
     error("Not a valid algorithm : " * algorithm)
