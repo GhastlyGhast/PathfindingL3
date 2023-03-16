283936afc9e9981c2a1abb908c347a0bf081c394
Virgil Marionneau <marionneau.virgil@gmail.com>
2023-03-16 20:03:02 +0100

    Added tie breaker back in

--- src/a_star.jl
+++ src/a_star.jl
@@ -5,7 +5,7 @@ import DataStructures.eq
 #Defining a lexicographic order for the priority queue
 struct AStarOrdering <: Ordering end
 
-lt(::AStarOrdering, (g1, h1), (g2, h2)) = h1 + g1 < h2 + g2 || h1 + g1 == h2 + g2 && h1 < h2
+lt(::AStarOrdering, (g1, h1, t1), (g2, h2, t2)) = h1 + g1 < h2 + g2 || h1 + g1 == h2 + g2 && h1 < h2 || h1 == h2 && g1 == g2 && t1 > t2
 
 function heuristic((y1,x1) :: Tuple{Int,Int}, (y2,x2) :: Tuple{Int,Int})
     dx = x2 - x1
@@ -13,6 +13,23 @@ function heuristic((y1,x1) :: Tuple{Int,Int}, (y2,x2) :: Tuple{Int,Int})
     return abs(dx) + abs(dy)
 end
    
+function tie_breaker((ny,nx) :: Tuple{Int, Int}, (cy,cx) :: Tuple{Int, Int}, (ty,tx) :: Tuple{Int, Int})
+    dy1 = ny - cy
+    dx1 = nx - cx
+    
+    dy2 = ty - cy
+    dx2 = tx - cx
+
+
+    n = sqrt(dy2^2 + dx2^2)
+
+    if n <= 0 
+        return 0
+    end
+
+    return Int(floor(10 * (dy1 * dy2 + dx1 * dx2) / n))
+end
+
 function a_star(map :: Matrix{TileType}, 
                 start :: Tuple{Int,Int}, 
                 target :: Tuple{Int,Int},
@@ -30,9 +47,9 @@ function a_star(map :: Matrix{TileType},
 
     current = start
 
-    open_cells = PriorityQueue{Tuple{Int,Int}, Tuple{Int,Int}}(AStarOrdering())
+    open_cells = PriorityQueue{Tuple{Int,Int}, Tuple{Int,Int,Int}}(AStarOrdering())
 
-    enqueue!(open_cells,start,(0,heuristic(start,target)))
+    enqueue!(open_cells,start,(0,heuristic(start,target),0))
 
     while current != target && length(open_cells) > 0
 
@@ -57,7 +74,7 @@ function a_star(map :: Matrix{TileType},
 
             newlength =  pathlength+cost
             if states[ny,nx] == Unvisited
-                enqueue!(open_cells, (ny,nx), (newlength, heuristic((ny,nx), target)))
+                enqueue!(open_cells, (ny,nx), (newlength, heuristic((ny,nx), target), tie_breaker((ny,nx), current, target)))
                 states[ny,nx] = Opened
                 parents[ny,nx] = current
                 pathlengths[ny,nx] = newlength
@@ -65,7 +82,7 @@ function a_star(map :: Matrix{TileType},
             elseif states[ny,nx] == Opened && newlength < pathlengths[ny,nx]
                 parents[ny,nx] = current
                 pathlengths[ny,nx] = newlength
-                open_cells[(ny,nx)] =  (newlength, heuristic((ny,nx), target))            
+                open_cells[(ny,nx)] =  (newlength, heuristic((ny,nx), target), tie_breaker((ny,nx), current, target))            
             end
         end
         
